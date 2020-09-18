a6f1c59edb396fbbb5a505303f7fe18839f9bf13
a3576419@163.com <a3576419@163.com>
2020-09-18 23:52:02 +0800

    导航head基本完成 只差移入的效果

--- src/components/common/head/Head.vue
+++ src/components/common/head/Head.vue
@@ -1,20 +1,48 @@
 <template>
   <div class="body">
-    <div class="head">
-      <!--    logo部分-->
-      <img src="~assets/img/home/logo.jpg" alt="" class="image">
-      <span>快乐课堂</span>
-      <div class="head-center">
-        <router-link style="margin-left: 60px" to="/home">首页</router-link>
-        <router-link style="margin-left: 40px" to="/jingpin">精品自学课&nbsp;&nbsp;&nbsp;</router-link>
-        <router-link style="margin-left: 40px" to="/xianmian">限免课程&nbsp;&nbsp;&nbsp;</router-link>
-        <router-link style="margin-left: 40px" to="/kuajing">跨境资讯&nbsp;&nbsp;&nbsp;</router-link>
-      </div>
-      <div class="head-right">
-        <router-link to="/login">登录</router-link>|
-        <router-link to="/logup">注册</router-link>
-      </div>
-    </div>
+<!--    <div class="head">-->
+<!--      &lt;!&ndash;    logo部分&ndash;&gt;-->
+<!--      <img src="~assets/img/home/logo.jpg" alt="" class="image">-->
+<!--      <span>快乐课堂</span>-->
+<!--      <div class="head-center">-->
+<!--        <router-link style="margin-left: 60px" to="/home">首页</router-link>-->
+<!--        <router-link style="margin-left: 40px" to="/jingpin">精品自学课&nbsp;&nbsp;&nbsp;</router-link>-->
+<!--        <router-link style="margin-left: 40px" to="/xianmian">限免课程&nbsp;&nbsp;&nbsp;</router-link>-->
+<!--        <router-link style="margin-left: 40px" to="/kuajing">跨境资讯&nbsp;&nbsp;&nbsp;</router-link>-->
+<!--      </div>-->
+<!--      <div class="head-right">-->
+<!--        <router-link to="/login">登录</router-link>|-->
+<!--        <router-link to="/logup">注册</router-link>-->
+<!--      </div>-->
+<!--    </div>-->
+
+    <table class="head">
+      <tr>
+        <td>
+          <table>
+            <tr>
+              <td>
+                <img src="~assets/img/home/logo.jpg" alt="" class="image">
+              </td>
+              <td>
+                <strong>快乐课堂</strong>
+              </td>
+              <td class="head-center">
+                <router-link style="margin-left: 60px" to="/home">首页</router-link>
+                <router-link style="margin-left: 40px" to="/jingpin">精品自学课</router-link>
+                <router-link style="margin: 40px" to="/xianmian">限免课程</router-link>
+                <router-link style="margin-right: 500px" to="/kuajing">跨境资讯</router-link>
+              </td>
+              <td class="head-right">
+                <router-link to="/login">登录 |</router-link>
+                <router-link to="/logup"> 注册</router-link>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+
     <router-view></router-view>
   </div>
 </template>
@@ -26,41 +54,61 @@
 </script>
 
 <style scoped>
-  .head {
-    width: 80%;
+  /*.head {*/
+  /*  width: 80%;*/
+  /*  margin: 0 auto;*/
+  /*  font-size: 18px;*/
+  /*  height: 80px;*/
+  /*  background-color: white;*/
+  /*  color: black;*/
+  /*  text-align: center;*/
+  /*}*/
+
+  /*.image {*/
+  /*  width: 60px;*/
+  /*  height: 60px;*/
+  /*  !*display: inline;*!*/
+  /*  text-align: center;*/
+  /*}*/
+
+  /*.head span {*/
+  /*  font-size: 25px;*/
+  /*}*/
+
+  /*.head-center {*/
+  /*  margin-left: 50px;*/
+  /*  display: inline;*/
+  /*  text-align: center;*/
+  /*}*/
+
+  /*.head-center .router-link-active {*/
+  /*  border-bottom: solid 3px pink;*/
+  /*  padding: 8px;*/
+  /*  font-size: 20px;*/
+  /*  margin-top: 10px;*/
+  /*}*/
+
+  /*.head-right {*/
+  /*  float: right;*/
+
+  /*}*/
+  .head{
     margin: 0 auto;
     font-size: 18px;
-    height: 80px;
-    background-color: white;
+  }
+  .head img{
+      width: 60px;
+      height: 60px;
+  }
+  .head strong{
+    font-size: 28px;
     color: black;
   }
-
-  .image {
-    width: 60px;
-    height: 60px;
-    /*display: inline;*/
-    text-align: center;
-  }
-
-  .head span {
-    font-size: 25px;
-  }
-
-  .head-center {
-    margin-left: 50px;
-    display: inline;
-  }
-
   .head-center .router-link-active {
-    border-bottom: solid 3px pink;
+    border-bottom: solid 4px mediumaquamarine;
     padding: 8px;
     font-size: 20px;
-    margin-top: 10px;
-  }
-
-  .head-right {
-    float: right;
-
+    color: black;
   }
 
 </style>
