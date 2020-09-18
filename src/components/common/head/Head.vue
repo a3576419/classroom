58eeec6b3cc67695279532869c52a165683711cd
a3576419@163.com <a3576419@163.com>
2020-09-18 22:09:50 +0800

    导航head登录和首页不在一行1

--- src/components/common/head/Head.vue
+++ src/components/common/head/Head.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="head banxin">
-    <!--    logo部分-->
-    <img src="~assets/img/home/logo.jpg" alt="" class="imgg">
-    <span>快乐课堂</span>
-    <div class="head-center">
-      <router-link style="margin-left: 60px" to="/home">首页&nbsp;&nbsp;&nbsp;</router-link>
-      <router-link style="margin-left: 40px" to="/jingpin">精品自学课&nbsp;&nbsp;&nbsp;</router-link>
-      <router-link style="margin-left: 40px" to="/xianmian">限免课程&nbsp;&nbsp;&nbsp;</router-link>
-      <router-link style="margin-left: 40px" to="/kuajing">跨境资讯&nbsp;&nbsp;&nbsp;</router-link>
-    </div>
-
-    <div class="head-right" >
-      <router-link  to="/login">登录</router-link>|
-      <router-link to="/logup">注册</router-link>
-      <!--      <router-view></router-view>-->
+  <div class="body">
+    <div class="head">
+      <!--    logo部分-->
+      <img src="~assets/img/home/logo.jpg" alt="" class="image">
+      <span>快乐课堂</span>
+      <div class="head-center">
+        <router-link style="margin-left: 60px" to="/home">首页</router-link>
+        <router-link style="margin-left: 40px" to="/jingpin">精品自学课&nbsp;&nbsp;&nbsp;</router-link>
+        <router-link style="margin-left: 40px" to="/xianmian">限免课程&nbsp;&nbsp;&nbsp;</router-link>
+        <router-link style="margin-left: 40px" to="/kuajing">跨境资讯&nbsp;&nbsp;&nbsp;</router-link>
+      </div>
+      <div class="head-right">
+        <router-link to="/login">登录</router-link>|
+        <router-link to="/logup">注册</router-link>
+      </div>
     </div>
     <router-view></router-view>
   </div>
@@ -26,32 +26,41 @@
 </script>
 
 <style scoped>
-  .head{
+  .head {
+    width: 80%;
+    margin: 0 auto;
     font-size: 18px;
-    /*height:40px;*/
+    height: 80px;
     background-color: white;
     color: black;
   }
-  .imgg{
+
+  .image {
     width: 60px;
     height: 60px;
+    /*display: inline;*/
+    text-align: center;
   }
-  .head span{
+
+  .head span {
     font-size: 25px;
   }
-  .head-center{
+
+  .head-center {
     margin-left: 50px;
     display: inline;
   }
-  .head-center .router-link-active{
+
+  .head-center .router-link-active {
     border-bottom: solid 3px pink;
     padding: 8px;
     font-size: 20px;
     margin-top: 10px;
   }
-  .head-right{
+
+  .head-right {
     float: right;
-    right: 20px;
+
   }
 
 </style>
